f534d0721970769d5557fb56e2b379ae098bfa5d
tomlenbzh <thomas.lenoel@epitech.eu>
2019-02-10 14:50:38 +0100

    Add Input through ranking component

---
+++ src/app/Service/ranking.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Response } from './../Models/response';
+import { HttpClient } from '@angular/common/http';
+
+const ApiUrl = environment.apiUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RankingService {
+
+  constructor(private http: HttpClient) { }
+
+  getRanking() {
+    return this.http.get(ApiUrl + 'top');
+  }
+}
