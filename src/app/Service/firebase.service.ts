39bb763f21fe533f68b29857209d143a754827e1
Alexandre <alexandre@usts.fr>
2019-02-14 17:28:42 +0100

    boomer

--- src/app/Service/firebase.service.ts
+++ src/app/Service/firebase.service.ts
@@ -9,9 +9,6 @@ export class FirebaseService {
   constructor(private storage: AngularFireStorage) { }
 
   getImageProfil(pseudo) {
-    const defaultImage =
-      'https://images.pexels.com/photos/132037/pexels-photo-132037.jpeg?cs=srgb&dl=beach-blur-boardwalk-132037.jpg&fm=jpg';
-
     return this.storage.ref(pseudo).getDownloadURL();
   }
 }
