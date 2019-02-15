802325685d03e9a87d214e1bd5d9a2925fae2b8d
tomlenbzh <thomas.lenoel@epitech.eu>
2019-02-15 17:57:01 +0100

    splash + icon + remove auto authentication

--- src/app/Pages/authentication/authentication.page.ts
+++ src/app/Pages/authentication/authentication.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Keyboard } from '@ionic-native/keyboard/ngx';
 
 import { NetworkService } from '../../Service/network.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-authentication',
@@ -13,7 +14,7 @@ export class AuthenticationPage {
 
   showComponent = true;
 
-  constructor(private keyboard: Keyboard, private networkService: NetworkService) {
+  constructor(private keyboard: Keyboard, private networkService: NetworkService, private router: Router) {
     this.networkService.checkNetworkQuality();
   }
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,35 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'authentication', pathMatch: 'full' },
-  { path: 'authentication', loadChildren: './Pages/authentication/authentication.module#AuthenticationPageModule' },
-  { path: 'home', loadChildren: './Pages/home/home.module#HomePageModule', canActivate: [AuthGuard] },
-  { path: 'room', loadChildren: './Pages/room/room.module#RoomPageModule', canActivate: [AuthGuard] },
-  { path: 'profile', loadChildren: './Pages/profil/profil.module#ProfilPageModule', canActivate: [AuthGuard] },
-  { path: 'scores', loadChildren: './Pages/scores/scores.module#ScoresPageModule', canActivate: [AuthGuard] },
+  {
+    path: '',
+    redirectTo: 'authentication',
+    pathMatch: 'full',
+  },
+  {
+    path: 'authentication',
+    loadChildren: './Pages/authentication/authentication.module#AuthenticationPageModule',
+  },
+  {
+    path: 'home',
+    loadChildren: './Pages/home/home.module#HomePageModule',
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'room',
+    loadChildren: './Pages/room/room.module#RoomPageModule',
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'profile',
+    loadChildren: './Pages/profil/profil.module#ProfilPageModule',
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'scores',
+    loadChildren: './Pages/scores/scores.module#ScoresPageModule',
+    canActivate: [AuthGuard]
+  }
 ];
 
 @NgModule({
